410e7dd5a07847352bce4c519dc97552d5067078
Julian Camacho <julian.francama@gmail.com>
2023-04-28 22:26:35 -0300

    añadí voz

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function num() {
     const numeros = [];
   for (let i = 0; i < 10; i++) {
     const numero = Math.floor(Math.random() * 1000000000).toString().padStart(9, '0');
+    // const numeroSinCeros = numero.replace(/^0+/, ''); // Elimina todos los ceros a la izquierda del número
     const grupos = numero.match(/.{1,3}/g);
     const numeroConPuntos = grupos.join('.');
     numeros.push(numeroConPuntos);
@@ -17,4 +18,14 @@ function num() {
 function print() {
     const numeros = num();
     list.innerHTML = '<ul>' + numeros.map(numero => '<li>' + numero + '</li>').join('') + '</ul>';
+    return numeros;
 }
+
+function leerNumeros() {
+    const numeros = print();
+    let texto = '';
+    for (let i = 0; i < numeros.length; i++) {
+      texto += numeros[i] + '. ';
+    }
+    responsiveVoice.speak(texto, 'Spanish Female'); 
+  }
